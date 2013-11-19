7768b90875403f4d0f3f986311d643917b3c269b
Alexander Loshchilov <aloschil@gmail.com>
2013-11-19 14:49:27 +0400

    ADD: pitch roll yaw added via decomposeProjectionMatrix

--- main.cxx
+++ main.cxx
@@ -1,13 +1,3 @@
-/*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * Written (W) 2012 Michal Uricar
- * Copyright (C) 2012 Michal Uricar
- */
-
 #include <cv.h>
 #include <cvaux.h>
 #include <highgui.h>
@@ -18,40 +8,69 @@
 #include <vector>
 
 #include "flandmark_detector.h"
-#include "jsoncpp/json/json.h"
+#include "json/json.h"
 
 using namespace std;
 
 /*
-*/
-void drawVectorFromFaceCenter( CvArr *orig,  double v[4] , const Json::Value &currentFace, CvScalar color, const cv::Matx34d &Rt, const cv::Mat &cameraMatrix) {
-    cv::Matx41d zeroPointOnMesh;
-    zeroPointOnMesh(0, 0) = 0;
-    zeroPointOnMesh(1, 0) = 0;
-    zeroPointOnMesh(2, 0) = 0;
-    zeroPointOnMesh(3, 0) = 1;
-    cv::Matx31d zeroPointOnImage = cv::Matx33d(cameraMatrix)*Rt*zeroPointOnMesh;
-    cout << "zeroPointOnImage = " << zeroPointOnImage << endl;
-
-    cv::Matx41d minusOnePointOnMesh;
-    minusOnePointOnMesh(0, 0) = v[0];
-    minusOnePointOnMesh(1, 0) = v[1];
-    minusOnePointOnMesh(2, 0) = v[2];
-    minusOnePointOnMesh(3, 0) = v[3];
-    cv::Matx31d minusOnePointOnImage = cv::Matx33d(cameraMatrix)*Rt*minusOnePointOnMesh;
-
+ */
+void drawVectorFromFaceCenter( CvArr *orig,
+                              double v[4] ,
+                              const Json::Value &currentFace,
+                              CvScalar color,
+                              const cv::Matx34d &Rt,
+                              const cv::Mat &cameraMatrix) {
+    cv::Matx41d originOnMesh;
+    originOnMesh(0, 0) = 0;
+    originOnMesh(1, 0) = 0;
+    originOnMesh(2, 0) = 0;
+    originOnMesh(3, 0) = 1;
+    cv::Matx31d originOnImage = Rt*originOnMesh;
+    
+    cv::Matx41d endpointOnMesh;
+    endpointOnMesh(0, 0) = v[0];
+    endpointOnMesh(1, 0) = v[1];
+    endpointOnMesh(2, 0) = v[2];
+    endpointOnMesh(3, 0) = v[3];
+    cv::Matx31d endpointOnImage = Rt*endpointOnMesh;
+    
     cv::Mat normalizedPointOnImage;
-
-    cv::normalize(cv::Mat(minusOnePointOnImage - zeroPointOnImage ),normalizedPointOnImage);
+    
+    cv::normalize(cv::Mat(endpointOnImage - originOnImage), normalizedPointOnImage);
     normalizedPointOnImage = normalizedPointOnImage * 100;
-
+    
     int end_x = cv::Vec3d(normalizedPointOnImage)(0)
-             + currentFace["face-center"]["x"].asFloat();
+    + currentFace["face-center"]["x"].asDouble();
     int end_y = cv::Vec3d(normalizedPointOnImage)(1) +
-            currentFace["face-center"]["y"].asFloat();
+    currentFace["face-center"]["y"].asDouble();
+    
+    cvLine(orig,
+           cvPoint(currentFace["face-center"]["x"].asDouble(),
+                   currentFace["face-center"]["y"].asDouble()),
+           cvPoint(end_x, end_y), color);
+}
 
-    cvLine(orig, cvPoint(currentFace["face-center"]["x"].asFloat(), currentFace["face-center"]["y"].asFloat()),
-            cvPoint(end_x, end_y), color);
+cv::Vec3d applyTransform(double v[4],
+                         const cv::Matx34d &Rt) {
+    cv::Matx41d originOnMesh;
+    originOnMesh(0, 0) = 0;
+    originOnMesh(1, 0) = 0;
+    originOnMesh(2, 0) = 0;
+    originOnMesh(3, 0) = 1;
+    cv::Matx31d originOnImage = Rt*originOnMesh;
+    
+    cv::Matx41d endpointOnMesh;
+    endpointOnMesh(0, 0) = v[0];
+    endpointOnMesh(1, 0) = v[1];
+    endpointOnMesh(2, 0) = v[2];
+    endpointOnMesh(3, 0) = v[3];
+    cv::Matx31d endpointOnImage = Rt*endpointOnMesh;
+    
+    cv::Mat normalizedPointOnImage;
+    
+    cv::normalize(cv::Mat(endpointOnImage - originOnImage), normalizedPointOnImage);
+    
+    return cv::Vec3d(normalizedPointOnImage);
 }
 
 Json::Value detectFaceInImage(IplImage *orig,
@@ -62,7 +81,7 @@ Json::Value detectFaceInImage(IplImage *orig,
                               double *landmarks)
 {
     Json::Value root(Json::arrayValue);
-
+    
     // Smallest face size.
     CvSize minFeatureSize = cvSize(40, 40);
     int flags =  CV_HAAR_DO_CANNY_PRUNING;
@@ -71,10 +90,10 @@ Json::Value detectFaceInImage(IplImage *orig,
     CvMemStorage* storage;
     CvSeq* rects;
     int nFaces;
-
+    
     storage = cvCreateMemStorage(0);
     cvClearMemStorage(storage);
-
+    
     // Detect all the faces in the greyscale image.
     rects = cvHaarDetectObjects(input,
                                 cascade,
@@ -84,12 +103,12 @@ Json::Value detectFaceInImage(IplImage *orig,
                                 flags,
                                 minFeatureSize);
     nFaces = rects->total;
-
+    
     double t = (double)cvGetTickCount();
-
+    
     for (int iface = 0; iface < (rects ? nFaces : 0); ++iface) {
         Json::Value currentFace;
-
+        
         CvRect *r = (CvRect*)cvGetSeqElem(rects, iface);
         
         bbox[0] = r->x;
@@ -101,38 +120,38 @@ Json::Value detectFaceInImage(IplImage *orig,
                          bbox,
                          model,
                          landmarks);
-
+        
         // display landmarks
         cvRectangle(orig, cvPoint(bbox[0], bbox[1]), cvPoint(bbox[2], bbox[3]), CV_RGB(255,0,0) );
-
+        
         Json::Value faceRect;
-
+        
         faceRect["top-left-x"] = bbox[0];
         faceRect["top-left-y"] = bbox[1];
         faceRect["bottom-right-x"] = bbox[2];
         faceRect["bottom-right-y"] = bbox[3];
-
+        
         currentFace["face-rect"] = faceRect;
-
+        
         cvRectangle(orig, cvPoint(model->bb[0], model->bb[1]), cvPoint(model->bb[2], model->bb[3]), CV_RGB(0,0,255) );
-
+        
         Json::Value flandmarkFaceRect;
-
+        
         flandmarkFaceRect["top-left-x"] = model->bb[0];
         flandmarkFaceRect["top-left-y"] = model->bb[1];
         flandmarkFaceRect["bottom-right-x"] = model->bb[2];
         flandmarkFaceRect["bottom-right-y"] = model->bb[3];
-
+        
         currentFace["flandmark-face-rect"] = flandmarkFaceRect;
-
+        
         cvCircle(orig, cvPoint((int)landmarks[0], (int)landmarks[1]), 3, CV_RGB(0, 0,255), CV_FILLED);
-
+        
         Json::Value faceCenter;
         faceCenter["x"] = landmarks[0];
         faceCenter["y"] = landmarks[1];
-
+        
         currentFace["face-center"] = faceCenter;
-
+        
         Json::Value canthusRl;
         Json::Value canthusLr;
         Json::Value mouthCornerR;
@@ -140,102 +159,104 @@ Json::Value detectFaceInImage(IplImage *orig,
         Json::Value canthusRr;
         Json::Value canthusLl;
         Json::Value nose;
-
+        
         for (int i = 2; i < 2*model->data.options.M; i += 2)
         {
             cvCircle(orig, cvPoint(int(landmarks[i]), int(landmarks[i+1])), 3, CV_RGB(255,0,0), CV_FILLED);
-
+            
             switch(i/2)
             {
-            case 1 :
-                canthusRl["x"] = landmarks[i];
-                canthusRl["y"] = landmarks[i+1];
-                currentFace["canthus-rl"] = canthusRl;
-                break;
-            case 2 :
-                canthusLr["x"] = landmarks[i];
-                canthusLr["y"] = landmarks[i+1];
-                currentFace["canthus-lr"] = canthusLr;
-                break;
-            case 3:
-                mouthCornerR["x"] = landmarks[i];
-                mouthCornerR["y"] = landmarks[i+1];
-                currentFace["mouth-corner-r"] = mouthCornerR;
-                break;
-            case 4:
-                mouthCornerL["x"] = landmarks[i];
-                mouthCornerL["y"] = landmarks[i+1];
-                currentFace["mouth-corner-l"] = mouthCornerL;
-                break;
-            case 5:
-                canthusRr["x"] = landmarks[i];
-                canthusRr["y"] = landmarks[i+1];
-                currentFace["canthus-rr"] = canthusRr;
-                break;
-            case 6:
-                canthusLl["x"] = landmarks[i];
-                canthusLl["y"] = landmarks[i+1];
-                currentFace["canthus-ll"] = canthusLl;
-                break;
-            case 7:
-                nose["x"] = landmarks[i];
-                nose["y"] = landmarks[i+1];
-                currentFace["nose"] = nose;
-                break;
+                case 1 :
+                    canthusRl["x"] = landmarks[i];
+                    canthusRl["y"] = landmarks[i+1];
+                    currentFace["canthus-rl"] = canthusRl;
+                    break;
+                case 2 :
+                    canthusLr["x"] = landmarks[i];
+                    canthusLr["y"] = landmarks[i+1];
+                    currentFace["canthus-lr"] = canthusLr;
+                    break;
+                case 3:
+                    mouthCornerR["x"] = landmarks[i];
+                    mouthCornerR["y"] = landmarks[i+1];
+                    currentFace["mouth-corner-r"] = mouthCornerR;
+                    break;
+                case 4:
+                    mouthCornerL["x"] = landmarks[i];
+                    mouthCornerL["y"] = landmarks[i+1];
+                    currentFace["mouth-corner-l"] = mouthCornerL;
+                    break;
+                case 5:
+                    canthusRr["x"] = landmarks[i];
+                    canthusRr["y"] = landmarks[i+1];
+                    currentFace["canthus-rr"] = canthusRr;
+                    break;
+                case 6:
+                    canthusLl["x"] = landmarks[i];
+                    canthusLl["y"] = landmarks[i+1];
+                    currentFace["canthus-ll"] = canthusLl;
+                    break;
+                case 7:
+                    nose["x"] = landmarks[i];
+                    nose["y"] = landmarks[i+1];
+                    currentFace["nose"] = nose;
+                    break;
             }
         }
-
+        
         cv::Mat rvec, tvec;
-
+        
         // landmarks coordiates from 3D model
-
-        float modX[6]={-0.23260, 1.19237,  (-0.60929 + 1.53890)/2.0,  -1.32542, 2.38696,  0.54571 };
-        float modY[6]={-6.82082, -6.80873, (-6.43984 + (-6.36571))/2.0, -6.62015, -6.48895, -8.01948};
-        float modZ[6]={71.57423, 71.47037, (68.69979 +  68.66721)/2.0, 71.55910, 71.48106, 69.76753};
-
+        
+        float modX[7]={-0.23260, 1.19237,  -0.60929, 1.53890,  -1.32542, 2.38696,  0.54571 };
+        float modY[7]={-6.82082, -6.80873, -6.43984, -6.36571, -6.62015, -6.48895, -8.01948};
+        float modZ[7]={71.57423, 71.47037, 68.69979, 68.66721, 71.55910, 71.48106, 69.76753};
+        
         vector<cv::Point3f> model_points;
-
-        for (int i=0;i<5;i++) {
+        
+        for (int i=0;i<7;i++) {
             model_points.push_back(cv::Point3f(modX[i],modY[i],modZ[i]));
         }
-
+        
         // detected landmark coordinates
-
-
-        float imX[6]={ currentFace["canthus-rl"]["x"].asFloat(),
-                       currentFace["canthus-lr"]["x"].asFloat(),
-                       (currentFace["mouth-corner-r"]["x"].asFloat() + currentFace["mouth-corner-l"]["x"].asFloat())/2.0,
-                       currentFace["canthus-rr"]["x"].asFloat(),
-                       currentFace["canthus-ll"]["x"].asFloat(),
-                       currentFace["nose"]["x"].asFloat()};
-        float imY[6]={ currentFace["canthus-rl"]["y"].asFloat(),
-                       currentFace["canthus-lr"]["y"].asFloat(),
-                       (currentFace["mouth-corner-r"]["y"].asFloat() + currentFace["mouth-corner-l"]["y"].asFloat())/2.0,
-                       currentFace["canthus-rr"]["y"].asFloat(),
-                       currentFace["canthus-ll"]["y"].asFloat(),
-                       currentFace["nose"]["y"].asFloat()};
-
+        
+        
+        float imX[7]={ currentFace["canthus-rl"]["x"].asDouble(),
+            currentFace["canthus-lr"]["x"].asDouble(),
+            currentFace["mouth-corner-r"]["x"].asDouble(),
+            currentFace["mouth-corner-l"]["x"].asDouble(),
+            currentFace["canthus-rr"]["x"].asDouble(),
+            currentFace["canthus-ll"]["x"].asDouble(),
+            currentFace["nose"]["x"].asDouble()};
+        float imY[7]={ currentFace["canthus-rl"]["y"].asDouble(),
+            currentFace["canthus-lr"]["y"].asDouble(),
+            currentFace["mouth-corner-r"]["y"].asDouble(),
+            currentFace["mouth-corner-l"]["y"].asDouble(),
+            currentFace["canthus-rr"]["y"].asDouble(),
+            currentFace["canthus-ll"]["y"].asDouble(),
+            currentFace["nose"]["y"].asDouble()};
+        
         vector<cv::Point2f> image_points;
-
-        for (int i=0;i<5;i++)
+        
+        for (int i=0;i<7;i++)
         {
             image_points.push_back(cv::Point2f(imX[i],imY[i]));
         }
-
-
+        
+        
         cv::Mat cameraMatrix = cv::Mat((CvMat*)cvLoad( "Intrinsics.xml" ));
         cv::Mat distCoeffs = cv::Mat((CvMat*)cvLoad( "Distortion.xml" ));
-
-
-        solvePnP(cv::Mat(model_points),
+        
+        
+        solvePnPRansac(cv::Mat(model_points),
                        cv::Mat(image_points),
-                       cameraMatrix, distCoeffs, rvec, tvec, false, CV_EPNP);
-
+                       cameraMatrix, distCoeffs, rvec, tvec, false);
+        
         cv::Matx33d R;
         cv::Rodrigues(rvec, R);
-
+        
         cv::Matx34d Rt;
-
+        
         for(int i=0; i<3; i++)
         {
             for(int j=0; j<4; j++)
@@ -249,22 +270,43 @@ Json::Value detectFaceInImage(IplImage *orig,
                 }
             }
         }
-
+        
         double minusYVector[4] = {0, -1, 0, 1};
         double xVector[4] = {1, 0, 0, 1};
         double yVector[4] = {0, 1, 0, 1};
         double zVector[4] = {0, 0, 1, 1};
-
-        drawVectorFromFaceCenter(orig, minusYVector, currentFace, CV_RGB(255, 255, 255), Rt, cameraMatrix);
-        drawVectorFromFaceCenter(orig, xVector, currentFace, CV_RGB(255, 0, 0), Rt, cameraMatrix);
-        drawVectorFromFaceCenter(orig, yVector, currentFace, CV_RGB(0, 255, 0), Rt, cameraMatrix);
-        drawVectorFromFaceCenter(orig, zVector, currentFace, CV_RGB(0, 0, 255), Rt, cameraMatrix);
-
+        
+        if(applyTransform(minusYVector, Rt)(1) >= 0) {
+            drawVectorFromFaceCenter(orig, minusYVector, currentFace, CV_RGB(255, 255, 255), Rt, cameraMatrix);
+            drawVectorFromFaceCenter(orig, xVector, currentFace, CV_RGB(255, 0, 0), Rt, cameraMatrix);
+            drawVectorFromFaceCenter(orig, yVector, currentFace, CV_RGB(0, 255, 0), Rt, cameraMatrix);
+            drawVectorFromFaceCenter(orig, zVector, currentFace, CV_RGB(0, 0, 255), Rt, cameraMatrix);
+        } else {
+            cv::Matx44d reverseY = cv::Matx44d::zeros();
+            reverseY(0,0) = 1;
+            reverseY(1,1) = -1;
+            reverseY(2,2) = 1;
+            reverseY(3,3) = 1;
+            Rt = Rt*reverseY;
+            drawVectorFromFaceCenter(orig, minusYVector, currentFace, CV_RGB(255, 255, 255), Rt, cameraMatrix);
+            drawVectorFromFaceCenter(orig, xVector, currentFace, CV_RGB(255, 0, 0), Rt, cameraMatrix);
+            drawVectorFromFaceCenter(orig, yVector, currentFace, CV_RGB(0, 255, 0), Rt, cameraMatrix);
+            drawVectorFromFaceCenter(orig, zVector, currentFace, CV_RGB(0, 0, 255), Rt, cameraMatrix);
+        }
+        
+        cv::Mat tmp,tmp1,tmp2,tmp3,tmp4,tmp5;
+        cv::Vec3d eav;
+        cv::decomposeProjectionMatrix(Rt, tmp, tmp1, tmp2, tmp3, tmp4, tmp5, eav);
+        
+        currentFace["roll"] = eav[0];
+        currentFace["pitch"] = eav[1];
+        currentFace["yaw"] = eav[3];
+        
         root.append(currentFace);
     }
     t = (double)cvGetTickCount() - t;
     int ms = cvRound( t / ((double)cvGetTickFrequency() * 1000.0) );
-
+    
     if (nFaces > 0)
     {
         printf("Faces detected: %d; Detection of facial landmark on all faces took %d ms\n", nFaces, ms);
@@ -273,11 +315,11 @@ Json::Value detectFaceInImage(IplImage *orig,
     }
     
     cvReleaseMemStorage(&storage);
-
+    
     return root;
 }
 
-int main( int argc, char** argv ) 
+int main( int argc, char** argv )
 {
     char flandmark_window[] = "flandmark_example1";
     double t;
@@ -301,17 +343,17 @@ int main( int argc, char** argv )
         printf("Couldnt load Face detector '%s'\n", faceCascadeFilename);
         exit(1);
     }
-
+    
     // ------------- begin flandmark load model
     t = (double)cvGetTickCount();
     FLANDMARK_Model * model = flandmark_init("flandmark_model.dat");
-
+    
     if (model == 0)
     {
         printf("Structure model wasn't created. Corrupted file flandmark_model.dat?\n");
         exit(1);
     }
-
+    
     t = (double)cvGetTickCount() - t;
     ms = cvRound( t / ((double)cvGetTickFrequency() * 1000.0) );
     printf("Structure model loaded in %d ms.\n", ms);
@@ -340,11 +382,11 @@ int main( int argc, char** argv )
         printf("Saving image to file %s...\n", argv[2]);
         cvSaveImage(argv[2], frame);
     }
-
-
+    
+    
     Json::StyledWriter writer;
     std::cout << root;
-
+    
     // cleanup
     free(bbox);
     free(landmarks);
